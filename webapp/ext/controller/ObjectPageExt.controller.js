f9920044d533bcc26e782572e46aa840232c4942
VasilKh <vasil.khiliuk@gmail.com>
2021-07-26 14:31:39 +0300

    Step 12 done

--- webapp/ext/controller/ObjectPageExt.controller.js
+++ webapp/ext/controller/ObjectPageExt.controller.js
@@ -1,7 +1,21 @@
+var id;
 sap.ui.controller("fioriapi.ext.controller.ObjectPageExt", {
+
     onInit: function (){
         this.extensionAPI.attachPageDataLoaded(function(oEvent){
             sap.m.MessageToast.show("Current Category ID is " + oEvent.context.getProperty("CategoryID") + ": " +oEvent.context.getProperty("CategoryName"));
+            id = oEvent.context.getProperty("CategoryID");
         });
+        
+    },
+
+    onActivateAction : function() {
+        this.getOwnerComponent().getModel().setProperty("/Categories(" + id + ")/StatusID", 2);
+        this.getOwnerComponent().getModel().submitChanges();
+        this.getOwnerComponent().getModel().refresh();
+    },
+
+    onDeleteAction : function() { 
+        sap.m.MessageToast.show("Hello World");
     }
 });
